7bc9798cc8fadc35be5223a24e671abe9df88f9e
Sviky7 <patrik.svikruha6@gmail.com>
2024-10-23 12:32:01 +0200

    funguje

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]/authOptions"
-import AuthHomeView from "@/components/AuthHomeView"
-import NonAuthHomeView from "@/components/NonAuthHomeView"
+import AuthHomeView from "@/sections/HomeViews/AuthHomeView"
+import NonAuthHomeView from "@/sections/HomeViews/NonAuthHomeView"
 
 export const metadata = { title: "Domov | ZoškaSnap" }
 
@@ -20,38 +20,3 @@ export default async function HomePage() {
 
 
 
-
-
-
-
-// import HomeIcon from "@mui/icons-material/Home";
-// import SearchIcon from "@mui/icons-material/Search";
-// import AddCircleIcon from "@mui/icons-material/AddCircle";
-// import NotificationsIcon from "@mui/icons-material/Notifications";
-// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-// import { useState } from "react";
-
-//   const [navValue, setNavValue] = useState(0);
-    // <Container maxWidth="sm" sx={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-    //   <Box component="main" sx={{ flexGrow: 1, paddingTop: 2 }}>
-    //     <Typography variant="h4" gutterBottom>
-    //       Vitajte na SnapZoška!
-    //     </Typography>
-    //     <Typography variant="body1">
-    //       Toto je vaša úvodná stránka. Tu nájdete príspevky od používateľov, pridáte vlastné alebo preskúmate nové príspevky.
-    //     </Typography>
-    //   </Box>
-
-    //   {/* Bottom Navigation */}
-    //   <BottomNavigation
-    //     value={navValue}
-    //     onChange={(event, newValue) => setNavValue(newValue)}
-    //     showLabels
-    //   >
-    //     <BottomNavigationAction label="Domov" icon={<HomeIcon />} />
-    //     <BottomNavigationAction label="Hľadať" icon={<SearchIcon />} />
-    //     <BottomNavigationAction label="Pridať" icon={<AddCircleIcon />} />
-    //     <BottomNavigationAction label="Notifikácie" icon={<NotificationsIcon />} />
-    //     <BottomNavigationAction label="Profil" icon={<AccountCircleIcon />} />
-    //   </BottomNavigation>
-    // </Container>
